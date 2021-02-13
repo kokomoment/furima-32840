c60b33c3f16c7cf613f5132c2987e322c528746f
kokomoment <catmilk0852@gmail.com>
2021-02-13 12:30:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width characters'}
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width characters'}
+  validates :birthday,        presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
+
 end
