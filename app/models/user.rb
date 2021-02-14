781daa0a3ba4612a9ca3036b9600a551fcf82a13
kokomoment <catmilk0852@gmail.com>
2021-02-14 13:03:21 +0900

    ユーザー管理機能ー修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width characters' }
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birthday
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -86,20 +86,40 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
+      it 'first_nameが漢字・平仮名・カタカナ以外' do
+        @user.first_name = 'abc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name Full-width characters")
+      end
       it 'last_nameが空' do
         @user.last_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'last_nameが漢字・平仮名・カタカナ以外' do
+        @user.last_name = 'abc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name Full-width characters")
       end
       it 'first_name_kanaが空' do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
+      it 'first_name_kanaが全角カタカナ以外' do
+        @user.first_name_kana = '山田'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana Full-width characters")
+      end
       it 'last_name_kanaが空' do
         @user.last_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it 'last_name_kanaが全角カタカナ以外' do
+        @user.last_name_kana = '太郎'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana Full-width characters")
       end
       it 'birthdayが空' do
         @user.birthday = ''
