5b391800e62b9e4dbac465fb9f41ed29ba965874
kokomoment <catmilk0852@gmail.com>
2021-02-16 21:30:49 +0900

    ActiveHash(アソシエーション記述)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :ship_fee
+  belongs_to :prefecture
+  belongs_to :ship_date
 end
