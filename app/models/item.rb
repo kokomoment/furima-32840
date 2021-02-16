a9733a838c384aa032ab69959b0f2cd58676a91d
kokomoment <catmilk0852@gmail.com>
2021-02-16 21:34:18 +0900

    ActiveHash(バリデーション記述)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
 
+  validates :category_id, :state_id, :ship_fee_id, :prefecture_id, :ship_date_id, numericality: { other_than: 1 }
+
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
