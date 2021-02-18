46c37c0e4a77f35b493bcff491618861bb855333
kokomoment <catmilk0852@gmail.com>
2021-02-18 20:33:39 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
 
   validates :category_id, :state_id, :ship_fee_id, :prefecture_id, :ship_date_id, numericality: { other_than: 1 }
+  with_option presence: true do
+    validates :product
+    validates :explain
+    validates :category_id
+    validates :state_id
+    validates :ship_fee_id
+    validates :prefecture_id
+    validates :ship_date_id
+    validates :price
+  end
 
   belongs_to :user
   has_one_attached :image
