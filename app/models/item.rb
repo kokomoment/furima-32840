9443e0395ed5c866d3cc5ca5cfc5d812d3a05c71
kokomoment <catmilk0852@gmail.com>
2021-02-20 08:37:33 +0900

    バリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,21 @@
 class Item < ApplicationRecord
-
-  validates :category_id, :state_id, :ship_fee_id, :prefecture_id, :ship_date_id, numericality: { other_than: 1 }
-  with_options presence: true do
-    validates :product
-    validates :explain
+  
+  with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :state_id
     validates :ship_fee_id
     validates :prefecture_id
     validates :ship_date_id
+  end
+
+  with_options presence: true do
+    validates :product
+    validates :explain
     validates :price
   end
+
+  validates :price, inclusion: { in: 300..9999999, message: 'Out of settintg range' },
+                    numericality: { message: 'Half-width number' }
 
   belongs_to :user
   has_one_attached :image
