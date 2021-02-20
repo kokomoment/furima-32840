121ec0e50d42dc1e479848ac63a6b464d5dbfe97
kokomoment <catmilk0852@gmail.com>
2021-02-20 13:12:38 +0900

    単体テストコードの雛形作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    product       { Faker::Lorem.word }
+    explain       { Faker::Lorem.sentence }
+    category_id   { Faker::Number.within(range: 2..10) }
+    state_id      { Faker::Number.within(range: 2..7) }
+    ship_fee_id   { Faker::Number.within(range: 2..3) }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    ship_date_id  { Faker::Number.within(range: 2..4) }
+    price         { Faker::Number.within(range: 300..9,999,999) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '商品出品がうまくいくとき' do
+      it 'image、product、explain、category_id、state_id、ship_fee_id、prefecture_id、ship_date_id、priceが存在する'
+        
+      end
+      it 'priceが半角数字' do
+       
+      end
+      it 'priceが300以上' do
+        
+      end
+      it 'priceが9,999,999以下' do
+
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it 'imageが空' do
+
+      end
+      it 'productが空' do
+
+      end
+      it 'explainが空' do
+
+      end
+      it 'category_idが1' do
+
+      end
+      it 'state_idが1' do
+
+      end
+      it 'ship_fee_idが1' do
+
+      end
+      it 'prefecture_idが1' do
+
+      end
+      it 'ship_date_idが1' do
+
+      end
+      it 'priceが空' do
+      
+      end
+      it 'priceが半角数字以外' do
+
+      end
+      it 'priceが300未満' do
+
+      end
+      it 'priceが10,000,000以上' do
+
+      end
+    end
+  end
 end
+
