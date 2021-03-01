dedd544019bf9f190cfb5085a02766194df9f1bb
kokomoment <catmilk0852@gmail.com>
2021-03-01 21:59:01 +0900

    addressコントローラーのcreateアクションの記述

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,21 @@
 class AddressesController < ApplicationController
 
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:address).prrmit(:postcode, :prefecture_id, :city, :block, :building, :phone_number)
   end
 end
