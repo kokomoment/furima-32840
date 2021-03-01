f5271197c5e184053360beee179359e53900e7e5
kokomoment <catmilk0852@gmail.com>
2021-03-01 22:02:58 +0900

    addressモデルにバリデーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
+  with_options presence: true do
+    validates :postcode
+    validates :city
+    validates :block
+    validates :phone_number
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+
   belongs_to :order
 end
