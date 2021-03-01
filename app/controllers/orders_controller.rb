8bd44eccc22e899407ff7871b789bd321daa86ea
kokomoment <catmilk0852@gmail.com>
2021-03-01 20:02:50 +0900

    createアクションの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,25 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      # pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(token: params[:token])
+  end
+
+  def pay_item
+  end
+  
 end
