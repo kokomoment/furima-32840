021b3727b2cf3518a1a7b60f0ae8b5776b137a07
kokomoment <catmilk0852@gmail.com>
2021-03-09 20:55:05 +0900

    クレジットカード決済機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,20 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
     validates :postcode
     validates :city
     validates :block
     validates :phone_number
+    validates :token
   end
 
   validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
-  validates :phone_number, format: { with: /[0-9]\d]{11,}/, message: "Input only number"}
+  validates :phone_number, format: { with: /\A\d{11}\z/, message: "Input only number"}
 
   def save
-    Order.create(user_id: current_user.id, item_id: params[:item_id])
+    Order.create(user_id: user_id, item_id: item_id)
     Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
   end
 end
