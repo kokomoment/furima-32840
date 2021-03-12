f77a95e27d973e7eaa34eabbe0416b913086fd55
kokomoment <catmilk0852@gmail.com>
2021-03-12 21:59:27 +0900

    テストコードの記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,7 +12,7 @@ class OrderAddress
 
   validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
-  validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/, message: "Input only number"}
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Input only number"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -14,30 +14,70 @@ describe OrderAddress do
 
     context '商品購入情報の保存がうまくいかないとき' do
       it 'postcodeが空' do
+        @order_address.postcode = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postcode can't be blank")
       end
 
       it 'postcodeにハイフンが含まれない' do
+        @order_address.postcode = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode Input correctly')
       end
 
       it 'postcodeが全角数字' do
+        @order_address.postcode = '１２３-４５６７'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode Input correctly')
+      end
+
+      it 'postcodeの数字の個数が少ない' do
+        @order_address.postcode = '123-456'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode Input correctly')
       end
 
       it 'prefecture_idが1' do
+        @order_address.prefecture_id = Prefecture.find(1)
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Prefecture Select')
       end
 
       it 'cityが空' do
+        @order_address.city = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
       end
 
       it 'blockが空' do
+        @order_address.block = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Block can't be blank")
       end
 
       it 'phone_numberが空' do
+        @order_address.phone_number = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
 
       it 'phone_numberが全角数字' do
+        @order_address.phone_number = '０１２３４５６７８９'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
       end
 
-      it 'tokenが空'
+      it 'phone_numberが10桁未満' do
+        @order_address.phone_number = '123456789'
+        @order_address.valid?
+        binding.pry
+        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
+      end
+
+      it 'tokenが空' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
