cef135780b2d543d9ac63811a72b4c8c1e88b6d9
kokomoment <catmilk0852@gmail.com>
2021-03-12 21:01:55 +0900

    テストコードを記述するファイルが違っていたため修正

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,40 +1,5 @@
-require 'rails_helper'
-
-describe OrderAddress do
-  before do
-    @order_address = FactoryBot.build(:order_address)
-  end
-
-  describe '商品購入情報の保存' do
-    context '商品購入情報の保存がうまくいくとき' do
-      it 'すべての値が正しく入力されていれば保存できる' do
-      end
-    end
-
-    context '商品購入情報の保存がうまくいかないとき' do
-      it 'postcodeが空' do
-      end
-
-      it 'postcodeにハイフンが含まれない' do
-      end
-
-      it 'postcodeが全角数字' do
-      end
-
-      it 'prefecture_idが1' do
-      end
-
-      it 'cityが空' do
-      end
-
-      it 'blockが空' do
-      end
-
-      it 'phone_numberが空' do
-      end
-
-      it 'phone_numberが全角数字' do
-      end
-    end
+FactoryBot.define do
+  factory :address do
+    
   end
 end
