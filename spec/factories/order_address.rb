40849483b37804b1501cfc4606da3424785f184e
kokomoment <catmilk0852@gmail.com>
2021-03-12 20:17:08 +0900

    テストコードのためのfactoryを記述

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+    postcode      { "123-4567" }
+    prefecture_id { 40 }
+    city          { "高知市" }
+    block         { "建物ハイツ" }
+    building      { "1-1" }
+    phone_number  { Faker::Number.leading_zero_number(digits: 11) }
+    order_id      { 1 }
+    user_id       { 1 }
+    item_id       { 1 }
+  end
+end
