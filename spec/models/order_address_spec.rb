78ea149a459c1a393f27a44f678b1bb1e5e43f56
kokomoment <catmilk0852@gmail.com>
2021-03-15 22:18:33 +0900

    商品購入機能ー修正1　テストにuser_idとitem_idが空では保存できないを追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -86,6 +86,18 @@ describe OrderAddress do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
+
+      it 'user_idが空' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
