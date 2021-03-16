fdffd05b75300fa84fd47d7cce2a7e56fc351726
kokomoment <catmilk0852@gmail.com>
2021-03-16 20:51:34 +0900

    商品購入機能ー修正2　商品購入ページに遷移における条件分岐を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,6 @@ class OrdersController < ApplicationController
   end
 
   def confirmation_seller
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
