8e075d86965f8cb191a148edcda23d36fa2c9b3b
kokomoment <catmilk0852@gmail.com>
2021-03-19 20:36:44 +0900

    空

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
 
+  
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
